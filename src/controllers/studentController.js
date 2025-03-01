520ea98af62f8f41bdf1f943295ceb394b1e428f
yuzamarde <yuzamarde@gmail.com>
2025-03-01 12:00:16 +0700

    initial commit

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -2,17 +2,28 @@ import userModel from "../models/userModel.js"
 import bcrypt from 'bcrypt';
 import fs from 'fs';
 import { mutateStudentSchema } from "../utils/schema.js";
+import courseModel from "../models/courseModel.js";
+import path from "path";
 
 export const getStudents = async (req, res) => {
     try {
         const students = await userModel.find({
             role: 'student',
             manager: req.user._id
+        }).select('name courses photo');
+
+        const photoUrl = process.env.APP_URL + '/uploads/students/'
+
+        const response = students.map((item) => {
+            return {
+                ...item.toObject(),
+                photo_url: photoUrl + item.photo
+            }
         })
 
         return res.json({
             message: 'Get Student success',
-            data: students
+            data: response
         })
     } catch (error) {
         console.log(error);
@@ -69,4 +80,92 @@ export const postStudent = async (req, res) => {
     }
 };
 
+export const updateStudent = async (req, res) => {
+    try {
+
+        const { id } = req.params
+        const body = req.body
+        const parse = mutateStudentSchema.partial({
+            password: true
+        }).safeParse(body)
+
+        if (!parse.success) {
+            const errorMessages = parse.error.issues.map((err) => err.message)
+
+            // Validate and handle file upload errors
+            if (req?.file?.path && fs.existsSync(req?.file?.path)) {
+                fs.unlinkSync(req?.file?.path);
+            }
+
+            return res.status(500).json({
+                message: 'Error Validation',
+                data: null,
+                errors: errorMessages,
+            });
+        }
+
+        const student = await userModel.findById(id)
+
+        const hashPassword = parse.data?.password
+            ? bcrypt.hashSync(parse.data.password, 12)
+            : student.password
+
+        await userModel.findByIdAndUpdate(id, {
+            name: parse.data.name,
+            email: parse.data.email,
+            password: hashPassword,
+            photo: req.file ? req.file?.filename : student.photo,
+        })
+
+
+        return res.json({
+            message: 'Update student success',
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Internal server error',
+        });
+    }
+};
+
+export const deleteStudent = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const student = await userModel.findById(id)
+
+        await courseModel.findOneAndUpdate({
+            students: id
+        }, {
+            $pull: {
+                students: id
+            }
+        })
+        const dirname = path.resolve()
+
+        // Construct the file path for the course thumbnail
+        const filePath = path.join(
+            dirname, 'public/uploads/students', student.photo
+        );
+
+        // Delete the thumbnail file if it exists
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+        }
+
+        await userModel.findByIdAndDelete(id)
+
+        return res.json({
+            message: 'Delete student sucess'
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Internal server error',
+        });
+
+    }
+}
 
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getStudents, postStudent } from "../controllers/studentController.js"
+import { deleteStudent, getStudents, postStudent, updateStudent } from "../controllers/studentController.js"
 import { verifyToken } from "../middlewares/verifyToken.js"
 import multer from "multer"
 import { fileStorage, fileFilter } from "../utils/multer.js"
@@ -13,5 +13,7 @@ const upload = multer({
 
 studentRoutes.get('/students', verifyToken, getStudents)
 studentRoutes.post('/students', verifyToken, upload.single('avatar'), postStudent)
+studentRoutes.put('/students/:id', verifyToken, upload.single('avatar'), updateStudent)
+studentRoutes.delete('/students/:id', verifyToken, deleteStudent)
 
 export default studentRoutes
